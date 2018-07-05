080b3d856039962f2d05cf710e7d57d4d0bfa48c
Pavel Stehule <pavel.stehule@gmail.com>
2018-07-05 19:30:29 +0200

    initial PostgreSQL 12 support

---
+++ postgresql12-plpgsql_check.spec
@@ -0,0 +1,86 @@
+%global pgmajorversion 12
+%global pginstdir /usr/pgsql-12
+%global sname plpgsql_check
+
+Name:		%{sname}_%{pgmajorversion}
+Version:	1.2.3
+Release:	1%{?dist}
+Summary:	Additional tools for plpgsql functions validation
+
+Group:		Applications/Databases
+License:	BSD
+URL:		https://github.com/okbob/plpgsql_check/archive/v%{version}.zip
+Source0:	plpgsql_check-%{version}.zip
+BuildRoot:	%{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildRequires:	postgresql%{pgmajorversion}-devel
+Requires:	postgresql%{pgmajorversion}
+
+%description
+The plpgsql_check is PostgreSQL extension with functionality for direct
+or indirect extra validation of functions in plpgsql language. It verifies
+a validity of SQL identifiers used in plpgsql code. It try to identify
+a performance issues.
+
+%prep
+%setup -q -n %{sname}-%{version}
+
+
+%build
+PATH="%{pginstdir}/bin;$PATH" ; export PATH
+CFLAGS="${CFLAGS:-%optflags}" ; export CFLAGS
+
+USE_PGXS=1 make %{?_smp_mflags}
+
+%install
+rm -rf %{buildroot}
+make USE_PGXS=1 DESTDIR=%{buildroot} install
+
+%clean
+rm -rf %{buildroot}
+
+%files
+%defattr(644,root,root,755)
+%doc README.md
+%{pginstdir}/lib/plpgsql_check.so
+%{pginstdir}/share/extension/plpgsql_check--1.2.sql
+%{pginstdir}/share/extension/plpgsql_check--1.0--1.1.sql
+%{pginstdir}/share/extension/plpgsql_check--1.1--1.2.sql
+%{pginstdir}/share/extension/plpgsql_check.control
+
+%changelog
+* Wed Jun 6 2018 - Pavel STEHULE <pavel.stehule@gmail.com> 1.2.3-1
+- PostgreSQL 11 support
+- detect hidden casts in expressions
+
+* Thu Oct 26 2017 - Pavel STEHULE <pavel.stehule@gmail.com> 1.2.2-1
+- never read variables detection
+- fix false alarm on MOVE command
+
+* Fri Sep 15 2017 - Pavel STEHULE <pavel.stehule@gmail.com> 1.2.1-1
+- missing RETURN detection
+- fix some bugs and false alarms
+- PostgreSQL 11 support
+
+* Fri Now 11 2016 - Pavel STEHULE <pavel.stehule@gmail.com> 1.2.0-1
+- support extra warnings - shadowed variables
+
+* Thu Aug 25 2016 - Pavel STEHULE <pavel.stehule@gmail.com> 1.0.5-1
+- minor fixes, support for PostgreSQL 10
+
+* Fri Apr 15 2016 - Pavel STEHULE <pavel.stehule@gmail.com> 1.0.4-1
+- support for PostgreSQL 9.6
+
+* Mon Oct 12 2015 - Pavel STEHULE <pavel.stehule@gmail.com> 1.0.3-1
+- fix false alarms of unused cursor variables
+- fix regress tests
+
+* Thu Jul 09 2015 - Pavel STEHULE <pavel.stehule@gmail.com> 1.0.2-2
+- bugfix release
+
+* Fri Dec 19 2014 - Pavel STEHULE <pavel.stehule@gmail.com> 0.9.3-1
+- fix a broken record field type checking
+- add check for assign to array field
+
+* Mon Aug 25 2014 - Pavel STEHULE <pavel.stehule@gmail.com> 0.9.1-1
+- Initial packaging
---
+++ sql/plpgsql_check_active-12.sql
@@ -0,0 +1,334 @@
+LOAD 'plpgsql';
+CREATE EXTENSION  IF NOT EXISTS plpgsql_check;
+
+-- check event trigger function 
+create or replace function f1() returns event_trigger as $$
+BEGIN
+    RAISE NOTICE 'test_event_trigger: % %', tg_event, tg_tag;
+END
+$$ language plpgsql;
+
+select * from plpgsql_check_function_tb('f1()');
+
+-- should fail
+create or replace function f1() returns event_trigger as $$
+BEGIN
+    RAISE NOTICE 'test_event_trigger: % %', tg_event, tg_tagX;
+END
+$$ language plpgsql;
+
+select * from plpgsql_check_function_tb('f1()');
+
+drop function f1();
+
+
+-- check event trigger function 
+create or replace function f1() returns event_trigger as $$
+BEGIN
+    RAISE NOTICE 'test_event_trigger: % %', tg_event, tg_tag;
+END
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()');
+
+-- should fail
+create or replace function f1() returns event_trigger as $$
+BEGIN
+    RAISE NOTICE 'test_event_trigger: % %', tg_event, tg_tagX;
+END
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()');
+
+drop function f1();
+
+create table t1tab(a int, b int);
+
+create or replace function f1()
+returns setof t1tab as $$
+begin
+  return next (10,20);
+  return;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()', performance_warnings => true);
+
+create or replace function f1()
+returns setof t1tab as $$
+begin
+  return next (10::numeric,20);
+  return;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()', performance_warnings => true);
+
+create or replace function f1()
+returns setof t1tab as $$
+declare a int; b int;
+begin
+  return next (a,b);
+  return;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()', performance_warnings => true);
+
+create or replace function f1()
+returns setof t1tab as $$
+declare a numeric; b int;
+begin
+  return next (a,b::numeric);
+  return;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('f1()', performance_warnings => true);
+
+drop function f1();
+
+create table t1(a int, b int);
+
+create or replace function fx()
+returns t2 as $$
+begin
+  return (10,20,30)::t1;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('fx()', performance_warnings => true);
+
+drop function fx();
+
+drop table t1tab;
+drop table t1;
+
+create or replace function fx()
+returns void as $$
+begin
+  assert exists(select * from foo);
+  assert false, (select boo from boo limit 1);
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('fx()', fatal_errors => false);
+
+create or replace function ml_trg()
+returns trigger as $$
+#option dump
+declare
+begin
+  if TG_OP = 'INSERT' then
+    if NEW.status_from IS NULL then
+      begin
+        -- performance issue only
+        select status into NEW.status_from
+           from pa
+          where pa_id = NEW.pa_id;
+        -- nonexist target value
+        select status into NEW.status_from_xxx
+           from pa
+          where pa_id = NEW.pa_id;
+      exception
+        when DATA_EXCEPTION then
+          new.status_from := 'DE';
+      end;
+    end if;
+  end if;
+  if TG_OP = 'DELETE' then return OLD; else return NEW; end if;
+exception
+  when OTHERS then
+    NULL;
+    if TG_OP = 'DELETE' then return OLD; else return NEW; end if;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('ml_trg()', 'ml', performance_warnings := true);
+
+create or replace function fx2()
+returns void as $$
+declare _pa pa;
+begin
+  select pa.id into _pa.id from pa limit 1;
+  select pa.pa_id into _pa.pa_id from pa limit 1;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('fx2()', performance_warnings := true);
+
+drop function fx2();
+
+create or replace function fx2()
+returns void as $$
+declare _pa pa;
+begin
+  _pa.id := (select pa.id from pa limit 1);
+  _pa.pa_id := (select pa.pa_id from pa limit 1);
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('fx2()', performance_warnings := true);
+
+drop function fx2();
+
+create type _exception_type as (
+  state text,
+  message text,
+  detail text);
+
+create or replace function f1()
+returns void as $$
+declare
+  _exception record;
+begin
+  _exception := NULL::_exception_type;
+exception when others then
+  get stacked diagnostics
+        _exception.state = RETURNED_SQLSTATE,
+        _exception.message = MESSAGE_TEXT,
+        _exception.detail = PG_EXCEPTION_DETAIL,
+        _exception.hint = PG_EXCEPTION_HINT;
+end;
+$$ language plpgsql;
+
+select f1();
+
+select * from plpgsql_check_function_tb('f1()');
+
+create or replace function f1()
+returns void as $$
+declare
+  _exception _exception_type;
+begin
+  _exception := NULL::_exception_type;
+exception when others then
+  get stacked diagnostics
+        _exception.state = RETURNED_SQLSTATE,
+        _exception.message = MESSAGE_TEXT,
+        _exception.detail = PG_EXCEPTION_DETAIL;
+end;
+$$ language plpgsql;
+
+select f1();
+
+select * from plpgsql_check_function_tb('f1()');
+
+drop function f1();
+
+drop type _exception_type;
+
+create type _exception_type as (
+  state text,
+  message text,
+  detail text);
+
+create or replace function f1()
+returns void as $$
+declare
+  _exception record;
+begin
+  _exception := NULL::_exception_type;
+exception when others then
+  get stacked diagnostics
+        _exception.state = RETURNED_SQLSTATE,
+        _exception.message = MESSAGE_TEXT,
+        _exception.detail = PG_EXCEPTION_DETAIL,
+        _exception.hint = PG_EXCEPTION_HINT;
+end;
+$$ language plpgsql;
+
+select f1();
+
+select * from plpgsql_check_function('f1()');
+
+drop function f1();
+drop type _exception_type;
+
+create or replace procedure proc(a int)
+as $$
+begin
+end;
+$$ language plpgsql;
+
+call proc(10);
+
+select * from plpgsql_check_function('proc(int)');
+
+create or replace procedure testproc()
+as $$
+begin
+  call proc(10);
+end;
+$$ language plpgsql;
+
+call testproc();
+
+select * from plpgsql_check_function('testproc()');
+
+-- should to fail
+create or replace procedure testproc()
+as $$
+begin
+  call proc((select count(*) from pg_class));
+end;
+$$ language plpgsql;
+
+call testproc();
+
+select * from plpgsql_check_function('testproc()');
+
+drop procedure proc(int);
+
+create procedure proc(in a int, inout b int, in c int)
+as $$
+begin
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('proc(int,int, int)');
+
+create or replace procedure proc(in a int, inout b int, in c int)
+as $$
+begin
+  b := a + c;
+end;
+$$ language plpgsql;
+
+select * from plpgsql_check_function('proc(int,int, int)');
+
+create or replace procedure testproc()
+as $$
+declare r int;
+begin
+  call proc(10, r, 20);
+end;
+$$ language plpgsql;
+
+call testproc();
+
+select * from plpgsql_check_function('testproc()');
+
+-- should to fail
+create or replace procedure testproc()
+as $$
+declare r int;
+begin
+  call proc(10, r + 10, 20);
+end;
+$$ language plpgsql;
+
+call testproc();
+
+select * from plpgsql_check_function('testproc()');
+
+create or replace procedure testproc(inout r int)
+as $$
+begin
+  call proc(10, r, 20);
+end;
+$$ language plpgsql;
+
+call testproc(10);
+
+select * from plpgsql_check_function('testproc(int)');
