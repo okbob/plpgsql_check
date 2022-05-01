e23b146e07f0da1874a1b349fcc2b09da2fa54d7
okbob@github.com <pavel.stehule@gmail.com>
2022-05-01 07:00:47 +0200

    fix warning for PostgreSQL 10

--- src/stmtwalk.c
+++ src/stmtwalk.c
@@ -24,7 +24,6 @@ static void check_stmts(PLpgSQL_checkstate *cstate, List *stmts, int *closing, L
 static PLpgSQL_stmt_stack_item * push_stmt_to_stmt_stack(PLpgSQL_checkstate *cstate);
 static void pop_stmt_from_stmt_stack(PLpgSQL_checkstate *cstate);
 static bool is_any_loop_stmt(PLpgSQL_stmt *stmt);
-static bool is_inside_protected_block(PLpgSQL_stmt_stack_item *current);
 static bool is_inside_exception_handler(PLpgSQL_stmt_stack_item *current);
 static PLpgSQL_stmt * find_nearest_loop(PLpgSQL_stmt_stack_item *current);
 static PLpgSQL_stmt * find_stmt_with_label(char *label, PLpgSQL_stmt_stack_item *current);
@@ -40,6 +39,12 @@ static void check_dynamic_sql(PLpgSQL_checkstate *cstate, PLpgSQL_stmt *stmt, PL
 #else
 
 static void check_dynamic_sql(PLpgSQL_checkstate *cstate, PLpgSQL_stmt *stmt, PLpgSQL_expr *query, bool into, PLpgSQL_row *row, PLpgSQL_rec *rec, List *params);
+
+#endif
+
+#if PG_VERSION_NUM >= 110000
+
+static bool is_inside_protected_block(PLpgSQL_stmt_stack_item *current);
 
 #endif
 
@@ -1588,6 +1593,8 @@ find_nearest_loop(PLpgSQL_stmt_stack_item *current)
 	return NULL;
 }
 
+#if PG_VERSION_NUM >= 110000
+
 /*
  * Returns true, when some outer block handles exceptions.
  * It is used for check of correct usage of COMMIT or ROLLBACK.
@@ -1610,6 +1617,8 @@ is_inside_protected_block(PLpgSQL_stmt_stack_item *current)
 
 	return false;
 }
+
+#endif
 
 /*
  * This is used for check of correct usage GET STACKED DIAGNOSTICS
