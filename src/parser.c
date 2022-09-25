3328e7f30a76b070a6a3d9ac47dac3c09d817430
okbob@github.com <pavel.stehule@gmail.com>
2022-09-25 10:55:19 +0200

    fix build on MSC

--- src/parser.c
+++ src/parser.c
@@ -1330,6 +1330,31 @@ comment_options_parser(char *str, plpgsql_check_info *cinfo)
 	while (_token);
 }
 
+#ifdef _MSC_VER
+
+static void *
+memmem(const void *haystack, size_t haystack_len,
+	   const void * const needle, const size_t needle_len)
+{
+	if (haystack == NULL) return NULL; // or assert(haystack != NULL);
+	if (haystack_len == 0) return NULL;
+	if (needle == NULL) return NULL; // or assert(needle != NULL);
+	if (needle_len == 0) return NULL;
+
+	for (const char *h = haystack;
+		 haystack_len >= needle_len;
+		 ++h, --haystack_len)
+	{
+		if (!memcmp(h, needle, needle_len))
+		{
+			return h;
+		}
+	}
+	return NULL;
+}
+
+#endif
+
 static void
 comment_options_parsecontent(char *str, size_t bytes, plpgsql_check_info *cinfo)
 {
