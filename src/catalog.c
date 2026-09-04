c36021d6546ea35356f07d5e475bf6d6c069c2e9
okbob@github.com <pavel.stehule@gmail.com>
2026-09-04 16:03:08 +0200

    fix reading namespaceid from already released SysCache tuple

--- src/catalog.c
+++ src/catalog.c
@@ -362,15 +362,17 @@ Oid
 plpgsql_check_get_op_namespace(Oid opno)
 {
 	HeapTuple	tp;
+	Oid			result = InvalidOid;
 
 	tp = SearchSysCache1(OPEROID, ObjectIdGetDatum(opno));
 	if (HeapTupleIsValid(tp))
 	{
 		Form_pg_operator optup = (Form_pg_operator) GETSTRUCT(tp);
 
+		result = optup->oprnamespace;
+
 		ReleaseSysCache(tp);
-		return optup->oprnamespace;
 	}
-	else
-		return InvalidOid;
+
+	return result;
 }
